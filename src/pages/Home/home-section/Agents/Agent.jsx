b860b14675c0ad63d8da9956f92ad67c2dbd046d
vuhai2310 <lvhai.19it4@vku.udn.vn>
2023-05-18 17:00:00 +0700

    15 - Loading 95%... Agent Section 💖✨

--- src/pages/Home/home-section/Agents/Agent.jsx
+++ src/pages/Home/home-section/Agents/Agent.jsx
@@ -14,6 +14,7 @@ SwiperCore.use([Navigation, FreeMode]);
 const Agent = (props) => {
   const [agents, setAgents] = useState([]);
   const [currentAgentIndex, setCurrentAgentIndex] = useState(0);
+  const [tabs, setTabs] = useState(0);
   const [agentDetails, setAgentDetails] = useState("");
 
   const swiperAgentRef = useRef(null);
@@ -47,6 +48,26 @@ const Agent = (props) => {
     },
     onSlideChange: (swiper) => {
       setCurrentAgentIndex(swiper.realIndex);
+      const fullPortrait = document.querySelector(".agent-full-portrait__img");
+      const bgTagName = document.querySelector(".tag-name");
+      fullPortrait.animate(
+        [
+          { transform: "translate(25%, 0)", opacity: 0 },
+          { transform: "translate(0, 0)", opacity: 1 },
+        ],
+        {
+          duration: 500,
+        }
+      );
+      bgTagName.animate(
+        [
+          { transform: "translate(-25%, 0)", opacity: 0 },
+          { transform: "translate(0, 0)", opacity: 0.5 },
+        ],
+        {
+          duration: 500,
+        }
+      );
     },
   };
 
@@ -64,6 +85,7 @@ const Agent = (props) => {
 
   const handleClickDetailsAgent = (index) => {
     swiperAgentRef.current.swiper?.slideTo(index);
+    console.log("agent" + index);
   };
 
   // const getAgentDetails = async (agent) => {
@@ -122,15 +144,140 @@ const Agent = (props) => {
             bgTagName={agents[currentAgentIndex].background}
             name={agents[currentAgentIndex].displayName}
             roleTitle={agents[currentAgentIndex]?.role?.displayName}
-            roleIcon={agents[currentAgentIndex]?.role?.displayIcon}
-            icon={agents[currentAgentIndex]?.abilities?.displayIcon}
-            // skillQ={agents[currentAgentIndex]?.skill}
-            // skillC={agents[currentAgentIndex]?.skill}
-            // skillE={agents[currentAgentIndex]?.skill}
-            // skillX={agents[currentAgentIndex]?.skill}
-            desc={agents[currentAgentIndex]?.description}
-            roleDesc={agents[currentAgentIndex]?.role?.description}
-          />
+          >
+            <div className="agent__abilities">
+              <div
+                className={
+                  tabs === 0
+                    ? "agent__abilities__item active"
+                    : "agent__abilities__item"
+                }
+                onClick={() => setTabs(0)}
+              >
+                <h3 className="agent__abilities__title">Info</h3>
+                <img
+                  src={agents[currentAgentIndex]?.role?.displayIcon}
+                  alt=""
+                  className="agent__abilities__icon"
+                />
+              </div>
+              <div
+                className={
+                  tabs === 1
+                    ? "agent__abilities__item active"
+                    : "agent__abilities__item"
+                }
+                onClick={() => setTabs(1)}
+              >
+                <h3 className="agent__abilities__title">C</h3>
+                <img
+                  src={agents[currentAgentIndex]?.abilities[2]?.displayIcon}
+                  alt=""
+                  className="agent__abilities__icon"
+                />
+              </div>
+              <div
+                className={
+                  tabs === 2
+                    ? "agent__abilities__item active"
+                    : "agent__abilities__item"
+                }
+                onClick={() => setTabs(2)}
+              >
+                <h3 className="agent__abilities__title">Q</h3>
+                <img
+                  src={agents[currentAgentIndex]?.abilities[0]?.displayIcon}
+                  alt=""
+                  className="agent__abilities__icon"
+                />
+              </div>
+              <div
+                className={
+                  tabs === 3
+                    ? "agent__abilities__item active"
+                    : "agent__abilities__item"
+                }
+                onClick={() => setTabs(3)}
+              >
+                <h3 className="agent__abilities__title">E</h3>
+                <img
+                  src={agents[currentAgentIndex]?.abilities[1]?.displayIcon}
+                  alt=""
+                  className="agent__abilities__icon"
+                />
+              </div>
+              <div
+                className={
+                  tabs === 4
+                    ? "agent__abilities__item active"
+                    : "agent__abilities__item"
+                }
+                onClick={() => setTabs(4)}
+              >
+                <h3 className="agent__abilities__title">X</h3>
+                <img
+                  src={agents[currentAgentIndex]?.abilities[3]?.displayIcon}
+                  alt=""
+                  className="agent__abilities__icon"
+                />
+              </div>
+            </div>
+            <div className="agent-introduce">
+              <div className={tabs === 0 ? "box active" : "box"}>
+                <h3 className="agent-introduce__top">
+                  {agents[currentAgentIndex].description}
+                </h3>
+                <div className="agent-introduce__bottom">
+                  <h3 className="agent-introduce__title">
+                    {agents[currentAgentIndex]?.role?.displayName}
+                  </h3>
+                  <p className="agent-introduce__subtitle">
+                    {agents[currentAgentIndex]?.role?.description}
+                  </p>
+                </div>
+              </div>
+              <div className={tabs === 1 ? "box active" : "box"}>
+                <div className="agent-introduce__bottom">
+                  <h3 className="agent-introduce__title">
+                    {agents[currentAgentIndex]?.abilities[2]?.displayName}
+                  </h3>
+                  <p className="agent-introduce__subtitle">
+                    {agents[currentAgentIndex]?.abilities[2]?.description}
+                  </p>
+                </div>
+              </div>
+              <div className={tabs === 2 ? "box active" : "box"}>
+                <div className="agent-introduce__bottom">
+                  <h3 className="agent-introduce__title">
+                    {agents[currentAgentIndex]?.abilities[0]?.displayName}
+                  </h3>
+                  <p className="agent-introduce__subtitle">
+                    {agents[currentAgentIndex]?.abilities[0]?.description}
+                  </p>
+                </div>
+              </div>
+              <div className={tabs === 3 ? "box active" : "box"}>
+                <div className="agent-introduce__bottom">
+                  <h3 className="agent-introduce__title">
+                    {agents[currentAgentIndex]?.abilities[1]?.displayName}
+                  </h3>
+                  <p className="agent-introduce__subtitle">
+                    {agents[currentAgentIndex]?.abilities[1]?.description}
+                  </p>
+                </div>
+              </div>
+              <div className={tabs === 4 ? "box active" : "box"}>
+                <div className="agent-introduce__bottom">
+                  <h3 className="agent-introduce__title">
+                    {agents[currentAgentIndex]?.abilities[3]?.displayName}
+                  </h3>
+                  <p className="agent-introduce__subtitle">
+                    {agents[currentAgentIndex]?.abilities[3]?.description}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </AgentDetails>
         )}
       </div>
     </HomeSection>
--- src/pages/Home/home-section/Agents/agent-details/AgentDetails.jsx
+++ src/pages/Home/home-section/Agents/agent-details/AgentDetails.jsx
@@ -21,55 +21,7 @@ const AgentDetails = (props) => {
           <div className="agent__info__wrapper">
             <p className="role-text">{props.roleTitle}</p>
             <h1 className="agent-name">{props.name}</h1>
-            <div className="agent__abilities">
-              <div className="agent__abilities__item active">
-                <h3 className="agent__abilities__title">Info</h3>
-                <img
-                  src={props.roleIcon}
-                  alt=""
-                  className="agent__abilities__icon"
-                />
-              </div>
-              <div className="agent__abilities__item">
-                <h3 className="agent__abilities__title">Q</h3>
-                <img
-                  src={props.skill}
-                  alt=""
-                  className="agent__abilities__icon"
-                />
-              </div>
-              <div className="agent__abilities__item">
-                <h3 className="agent__abilities__title">C</h3>
-                <img
-                  src={props.skill}
-                  alt=""
-                  className="agent__abilities__icon"
-                />
-              </div>
-              <div className="agent__abilities__item">
-                <h3 className="agent__abilities__title">E</h3>
-                <img
-                  src={props.skill}
-                  alt=""
-                  className="agent__abilities__icon"
-                />
-              </div>
-              <div className="agent__abilities__item">
-                <h3 className="agent__abilities__title">X</h3>
-                <img
-                  src={props.skill}
-                  alt=""
-                  className="agent__abilities__icon"
-                />
-              </div>
-            </div>
-            <div className="agent-introduce">
-              <h3 className="agent-introduce__top">{props.desc}</h3>
-              <div className="agent-introduce__bottom">
-                <h3 className="agent-introduce__title">{props.roleTitle}</h3>
-                <p className="agent-introduce__subtitle">{props.roleDesc}</p>
-              </div>
-            </div>
+            {props.children}
           </div>
         </div>
       </div>
