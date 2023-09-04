39028dc6b3b2935ee6df2d8e6055c0a5a2fbc948
vuhai2310 <lvhai.19it4@vku.udn.vn>
2023-09-04 16:22:06 +0700

    Update Maps Page & fix some bugs ✨

--- src/pages/Maps/Maps.jsx
+++ src/pages/Maps/Maps.jsx
@@ -6,18 +6,20 @@ import "swiper/swiper-bundle.css";
 import valorantAPI from "../../api/valorantAPI";
 
 import "./maps.scss";
+import HomeFooter from "../Home/home-footer/HomeFooter";
 
 SwiperCore.use([]);
 
 const Maps = () => {
   const [items, setItems] = useState([]);
   const [mapsIndex, setMapsIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
 
   useEffect(() => {
     const getItems = async () => {
       try {
         const res = await valorantAPI.getMaps();
-        const mapData = res.data;
+        const mapData = await res.data;
         console.log(mapData.data);
         setItems(mapData.data);
       } catch (err) {
@@ -30,30 +32,43 @@ const Maps = () => {
   const swiperOptions = {
     slidesPerView: 2.7,
     speed: 500,
-    // loop: true,
+    loop: true,
     onSlideChange: (swiper) => {
-      setMapsIndex(swiper.activeIndex);
+      setMapsIndex(swiper.realIndex);
     },
   };
 
   return (
-    <div className="maps-container">
-      <MapSection
-        bgImage={items[mapsIndex]?.splash}
-        name={items[mapsIndex]?.displayName}
-        coordinates={items[mapsIndex]?.coordinates}
-      />
-
-      <Swiper {...swiperOptions} id="mapSwiper">
-        {items.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className="map-img">
-              <img src={item.splash} alt="" className="map-img__splash" />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <>
+      <div className="maps-container">
+        <MapSection
+          bgImage={items[mapsIndex]?.splash}
+          name={items[mapsIndex]?.displayName}
+          coordinates={items[mapsIndex]?.coordinates}
+        />
+        <div className="maps-index">
+          {mapsIndex < 9 ? (
+            <span className="current-index">{`0${mapsIndex + 1}`}</span>
+          ) : (
+            <span className="current-index">{mapsIndex + 1}</span>
+          )}
+          <span className="divider">/</span>
+          <span className="total-num">{items.length}</span>
+        </div>
+        {items.length > 0 && (
+          <Swiper {...swiperOptions} id="mapSwiper">
+            {items.map((item, index) => (
+              <SwiperSlide key={index}>
+                <div className="map-img">
+                  <img src={item.splash} alt="" className="map-img__splash" />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+      </div>
+      <HomeFooter />
+    </>
   );
 };
 
